35d39036c86e26111f41002601338536461a123b
Piotr Prominski <piotr.prominski@gmail.com>
2017-04-03 10:57:53 +0200

    Innit

---
+++ Django/movies/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# class Person(models.Model):
+#     name = models.CharField(max_length=128)
+#
+# class Movie(models.Model):
+#     title = models.CharField(max_length=128)
+#     description = models.TextField()
+#     directors = models.ForeignKey(Person)
+#     actors = models.ManyToManyField(Person)
+#     year = models.IntegerField()
